8b76c0ecf43718ca998e846d2dc12b6f6b77d1c9
saheba chabra <saheba.chabra@hotmail.com>
2025-01-08 12:02:10 +0000

    Add containers for each blog post

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from .forms import PostForm
 
 
 class HomeView(ListView):
@@ -18,5 +19,6 @@ class ArticleDetailView(DetailView):
 
 class AddPostView(CreateView):
     model = Post
+    form_class = PostForm
     template_name = 'add_post.html'
-    fields = '__all__'
+   
